eb354e808452c1f8e00231b0838924e6e09c3b53
squishcakesart <squish.cakes.art@gmail.com>
2026-01-11 17:20:00 +0000

    Added backgrounds to cards and descriptions

---
+++ src/date_setter.js
@@ -0,0 +1,10 @@
+/**
+ * Sets the year of the copywrite footer.
+ */
+function set_footer_year() 
+{
+  const footer = document.getElementById('copyright');
+  footer.textContent = `Copyright SquishCakesArt ${(new Date()).getFullYear()}`;
+}
+
+set_footer_year();
